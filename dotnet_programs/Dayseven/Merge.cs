3571f9e723af11e8efa2bf44e2af22ac34f6b457
Ashi Gupta <guptaashi655@gmail.com>
2025-12-29 11:38:10 +0530

    Day 7 C# programs

--- dotnet_programs/Dayseven/Merge.cs
+++ dotnet_programs/Dayseven/Merge.cs
@@ -6,10 +6,6 @@ public class Merge
         int[] arr1={1,2,3,4};
         int[] arr2={2,3,4,5};
         int[] arr3=new int[arr1.Length+arr2.Length];
-        HashSet<int> hs=new HashSet<int>();
-        foreach(int x in hs)
-        {
-            
-        }
+     
     }
 }
