4df82e4aac61f71e74e8f21ea1ecd395321a2b9f
Ashi Gupta <guptaashi655@gmail.com>
2025-12-29 09:52:31 +0530

    Day 6 , 7

---
+++ dotnet_programs/Daily_Assessments/Enterprises/Program.cs
@@ -0,0 +1,9 @@
+﻿Programs.Task1_ProductPriceAnalysis();
+        Programs.Task2_BranchSales();
+        Programs.Task3_JaggedPerformance();
+        Programs.Task4_CustomerCleaning();
+        Programs.Task5_TransactionFiltering();
+        Programs.Task6_ProcessFlow();
+        Programs.Task7_LegacyRisk();
+
+        Console.WriteLine("\n*********** PROGRAM COMPLETED SUCCESSFULLY ***********");
