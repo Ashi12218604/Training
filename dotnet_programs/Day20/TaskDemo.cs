2e4618c132505ff1db37f85c9d3621da2ae73115
Ashi Gupta <guptaashi655@gmail.com>
2026-01-15 12:37:36 +0530

    Tasks and threading

--- dotnet_programs/Day20/TaskDemo.cs
+++ dotnet_programs/Day20/TaskDemo.cs
@@ -15,5 +15,12 @@ public class TaskDemo
         {
             Console.WriteLine(ex.InnerExceptions[0].Message);
         }
+        Task t1=Task.Run(() =>Console.WriteLine("Task 1"));
+                Task t2=Task.Run(() =>Console.WriteLine("Task 2"));
+                Task.WhenAll(t1,t2).ContinueWith(t => Console.WriteLine("All tasks completed"));
+
+Task <int> t3= Task.Run(() => 42);
+        t3.ContinueWith(resultTask => Console.WriteLine($"Result: {resultTask.Result}")).Wait();
+   
     }
 }
