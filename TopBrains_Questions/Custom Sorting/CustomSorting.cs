4fcb1ff72ce6a7ea1a9a9cf1cf3c196860c607ed
Ashi Gupta <118842314+Ashi12218604@users.noreply.github.com>
2026-01-29 15:06:06 +0530

    Update CustomSorting.cs

--- TopBrains_Questions/Custom Sorting/CustomSorting.cs
+++ TopBrains_Questions/Custom Sorting/CustomSorting.cs
@@ -2,15 +2,15 @@ using System;
 using System.Collections.Generic;
     public class Student
     {
-        public string name { get; set; }
-        public int age { get; set; }
-        public int marks { get; set; }
+        public string name {get;set;}
+        public int age {get;set;}
+        public int marks {get;set;}
     }
-    public class StudentComparer : IComparer<Student>
+    public class StudentComparer:IComparer<Student>
     {
         public int Compare(Student x, Student y)
         {
-            if (x.marks != y.marks)
+            if (x.marks!= y.marks)
                 return y.marks.CompareTo(x.marks);
             return x.age.CompareTo(y.age);
         }
@@ -21,14 +21,13 @@ using System.Collections.Generic;
         {
             List<Student> students = new List<Student>
             {
-                new Student { name = "Ashi Gupta", age = 22, marks = 99 },
-                new Student { name = "Sanjana Velma", age = 21, marks = 98 },
-                new Student { name = "Arnav Gupta", age = 19, marks = 95 },
-                new Student { name = "Raima Shihab", age = 22, marks = 98 },
-                new Student { name = "Shahid Shaik", age = 20, marks = 98 }
+                new Student { name = "Ashi Gupta", age = 22, marks = 99},
+                new Student { name = "Sanjana Velma", age = 21, marks = 98},
+                new Student { name = "Arnav Gupta", age = 19, marks = 95},
+                new Student { name = "Raima Shihab", age = 22, marks = 98},
+                new Student { name = "Shahid Shaik", age = 20, marks = 98}
             };
             students.Sort(new StudentComparer());
-
             foreach (var s in students)
             {
                 Console.WriteLine($"{s.name} - Age:{s.age}, Marks:{s.marks}");
