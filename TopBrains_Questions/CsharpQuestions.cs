bdb7fa6b698f66ba28567049f5002d44059131c6
Ashi Gupta <118842314+Ashi12218604@users.noreply.github.com>
2026-01-30 12:35:39 +0530

    Add IsPrime method to Solution class
    Implement IsPrime method to check if a number is prime.

---
+++ TopBrains_Questions/CsharpQuestions.cs
@@ -0,0 +1,17 @@
+public class Solution
+{
+    public bool IsPrime(int n)
+    {
+        int c=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            c++;
+        }
+        if(c==2)
+        return true;
+        else 
+        return false;
+        // Write code Here
+    }
+}
