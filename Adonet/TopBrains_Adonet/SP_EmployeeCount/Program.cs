41c1cd5010588ad6897ec9505eb7d63c647b568f
Ashi Gupta <guptaashi655@gmail.com>
2026-02-24 16:09:54 +0530

    EmployeeCount

---
+++ Adonet/TopBrains_Adonet/SP_EmployeeCount/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+class Employee_Count
+{
+    static void Main()
+    {
+        string cs =
+            "Data Source=localhost\\SQLEXPRESS;Initial Catalog=LPU;Integrated Security=True;TrustServerCertificate=True";
+
+        using SqlConnection con = new SqlConnection(cs);
+        using SqlCommand cmd = new SqlCommand("sp_GetTotalEmployeeCount", con);
+
+        cmd.CommandType = CommandType.StoredProcedure;
+
+        SqlParameter p = new SqlParameter("@TotalCount", SqlDbType.Int);
+        p.Direction = ParameterDirection.Output;
+        cmd.Parameters.Add(p);
+
+        con.Open();
+        cmd.ExecuteNonQuery();
+
+        Console.WriteLine("Employee Count: " + p.Value);
+    }
+}
